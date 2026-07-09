5672355612cd03b99397cf93c7b8b9e79afb2af7
ashwinchena <ashwinchena@gmail.com>
2026-07-09 09:54:30 +0530

    h

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import useDashboardStore from '../stores/dashboardStore';
 import useAuthStore from '../stores/authStore';
