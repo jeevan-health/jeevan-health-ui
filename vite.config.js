62394afd681c50be4a5a49810233127c67c80d48
ashwinchena <ashwinchena@gmail.com>
2026-07-10 23:37:45 +0530

    gf

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
